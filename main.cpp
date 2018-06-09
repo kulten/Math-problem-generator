c1d4c7c6e7208fb4ec53c7a1efa2f9c63e04c812
Delvin Cruze <delvinc82@gmail.com>
2018-06-09 13:18:42 +0530

    supports addition

--- main.cpp
+++ main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
-
+#include<random>
+#include<conio.h>
+#include<chrono>
+#include<ctime>
 using namespace std;
+void addition(int n,int r1, int r2)
+{
+    int a,b,c,d;
+  if(r1 > r2)
+  {
+      int t = r2;
+      r2 = r1;
+      r1 = t;
+  }
+  chrono::system_clock::time_point t = chrono::system_clock::now();
+  time_t tp = chrono::system_clock::to_time_t(t);
+  mt19937 ab(tp);
+  uniform_int_distribution<> range(r1, r2);
+  for(int i = 1; i <= n; i++)
+  {
 
+        a = range(ab);
+        b = range(ab);
+        c = a + b;
+        cout<<"\nSum number: "<<i<<endl;
+        cout<<a<<" + "<<b<<" = ?"<<endl;
+        cout<<"What's the answer?\n";
+        cin>>d;
+        while(d!=c)
+        {
+            cout<<"\nWrong answer, try again\n";
+            cin>>d;
+        }
+        cout<<"\Good job!\n";
+  }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int x,y,z;
+    cout<<"How many sums?\n";
+    cin>>x;
+    cout<<"\nWhat range should they be in?\n";
+    cout<<"First number: ";
+    cin>>y;
+    cout<<"\nSecond number: ";
+    cin>>z;
+    cout<<"\ngot it! "<<x<<" math problems coming right up!\n";
+    addition(x,y,z);
+    getch();
 }
